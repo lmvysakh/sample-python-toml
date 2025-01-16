0ab494fcefdb409b52eac58658b5873a852a9731
lmvysakh <lmvysakh@github.com>
2025-01-16 18:22:26 +0530

    main

---
+++ main.py
@@ -0,0 +1,9 @@
+# main.py
+
+def main():
+    # This is the main function that will be executed when the script runs
+    print("Hello, World!")
+
+if __name__ == "__main__":
+    # This ensures that the main function is called when the script is executed
+    main()
